b357ea84b998f6cfc203bb8d819b6b1412d64d9f
ikshvaku-vns <ankitanand101996@gmail.com>
2024-12-18 19:39:27 +0530

    Initial commit

---
+++ seeds/index.js
@@ -0,0 +1,53 @@
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDb = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
+    const camp = new Campground({
+      // Your USER ID
+      author: "675c3394cbda739cd5e07ec5",
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: "Some random Texts",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+          filename: "YelpCamp/ahfnenvca4tha00h2ubt",
+        },
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png",
+          filename: "YelpCamp/ruyoaxgf72nzpi4y6cdi",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
+
+seedDb().then(() => {
+  mongoose.connection.close();
+});
